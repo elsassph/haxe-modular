3e9b31e8a38acb23e2fc85aadf91ecf5d877604b
Philippe Elsass <philippe.elsass@gmail.com>
2018-01-16 09:13:14 +0000

    Add /doc in haxelib package

--- releaseHaxelib.sh
+++ releaseHaxelib.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -f haxe-modular.zip
-zip -r haxe-modular.zip src extraParams.hxml haxelib.json readme.md
+zip -r haxe-modular.zip src doc extraParams.hxml haxelib.json readme.md
 haxelib submit haxe-modular.zip
