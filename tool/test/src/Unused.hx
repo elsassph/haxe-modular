dbdefce8d0abd870ca8fa8215c97d6c92fb1c9a8
Philippe <philippe.elsass@gmail.com>
2018-01-28 18:22:24 +0000

    Bit of code refactoring without functional change (#51)
    Added 'release mode' test generation

--- tool/test/src/Unused.hx
+++ tool/test/src/Unused.hx
@@ -1,10 +1,19 @@
 package;
 
+import foo.Gee;
+import foo.Sub__M;
+
 @:keep
 class Unused
 {
 	public function new()
 	{
 		trace('I\'m not used');
+
+		Bundle.load(Sub__M).then(function(_) {
+			var s = new Sub__M();
+		});
+
+		var g = new Gee();
 	}
 }
