01b9e90e1385abd7b13a034f0bc3c988202ff6e6
Philippe Elsass <philippe.elsass@massive.co>
2018-04-06 12:32:25 +0100

    Release version 0.10.6

---
+++ tagRelease.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+VERSION=$1
+
+# Validate version
+if [ -z "$VERSION" ]; then
+    echo "No version specified"
+    exit 1
+fi
+if [ $(git tag -l "$VERSION") ]; then
+    echo "Tag $VERSION already exists"
+    exit 1
+fi
+
+# Change version in haxelib.json and package.json
+echo "Update version to $VERSION"
+PATTERN="s/\"version\": \"[0-9]+.[0-9]+.[0-9]+\"/\"version\": \"$VERSION\"/g"
+
+case "$(uname -s)" in
+   Darwin)
+     sed -E -i "" "$PATTERN" ./haxelib.json
+     sed -E -i "" "$PATTERN" ./package.json
+     ;;
+   *)
+     sed -E -i "$PATTERN" ./haxelib.json
+     sed -E -i "$PATTERN" ./package.json
+     ;;
+esac
+
+# Tag, commit and push to trigger a new CI release
+git tag $VERSION
+git commit -am "Release version $VERSION"
+# git push origin $VERSION
